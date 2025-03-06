6c1a2a839891c1582b6844d6b38ae4f776724f4b
Ignas Mikenas <mikenasignas@gmail.com>
2025-03-06 21:36:53 +0200

    add post request

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express       = require('express');
 const cors          = require('cors');
 const { Client }    = require('pg');
-
-const app   = express();
-const port  = 3000;
+const bcrypt        = require('bcrypt');
+const app           = express();
+const port          = 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -27,6 +27,26 @@ app.get('/users', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({ success: false, message: "Username and password required" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const sql = 'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *';
+        const result = await client.query(sql, [username, hashedPassword]);
+
+        res.json({ success: true, message: "User registered successfully", userId: result.rows[0].id });
+
+    } catch (error) {
+        console.error('Server Error:', error);
+        res.status(500).json({ success: false, message: "Server error", error: error.message });
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
